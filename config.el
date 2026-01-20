d4adb8912eb0a23cad460d0a672ea7b6e2905f1e
joao <joaobpedro@gmail.com>
2026-01-20 09:48:00 +0100

    my emacs config

---
+++ config.el
@@ -0,0 +1,126 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Inconsolata Nerd Font Mono" :size 22 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 18))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'naysayer)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+(setq confirm-kill-emacs nil)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; set default directory
+(setq-default default-directory "mnt")
+
+
+;; config harpoon
+;;You can use this hydra menu that have all the commands
+(map! :leader "h h" 'harpoon-quick-menu-hydra)
+(map! :leader "A" 'harpoon-add-file)
+
+;; And the vanilla commands
+(map! :leader "H c" 'harpoon-clear)
+(map! :leader "H f" 'harpoon-toggle-file)
+(map! :leader "1" 'harpoon-go-to-1)
+(map! :leader "2" 'harpoon-go-to-2)
+(map! :leader "3" 'harpoon-go-to-3)
+(map! :leader "4" 'harpoon-go-to-4)
+(map! :leader "5" 'harpoon-go-to-5)
+(map! :leader "6" 'harpoon-go-to-6)
+(map! :leader "7" 'harpoon-go-to-7)
+(map! :leader "8" 'harpoon-go-to-8)
+(map! :leader "9" 'harpoon-go-to-9)
+
+;; Drag stuff config
+(use-package! drag-stuff
+  :defer t
+  :init
+  (map! "<M-up>" #'drag-stuff-up
+        "<M-down>" #'drag-stuff-down
+        "<M-left>" #'drag-stuff-left
+        "<M-right>" #'drag-stuff-right))
+
+
+;; Customize which words are highlighted by hl-todo
+(after! hl-todo
+  (setq hl-todo-keywords
+        '(("TODO" . "#FF0000")   ; Red color for TODO
+          ("FIXME" . "#FF4500")  ; OrangeRed color for FIXME
+          ("NOTE" . "#0000FF")   ; Blue color for NOTE
+          ("EDIT" . "#FFA500")   ; New word 'EDIT' with Orange color
+          ("REVIEW" . "#800080") ; Review in Purple
+          ("HACK" . "#008000")   ; Hack in Green
+          ("HARDCODED" . "#008000")
+          )))
+
+
+;; change surrounf key map
+(map! :leader "c b" 'sp-rewrap-sexp)
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
